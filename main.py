3e8115155e0932ee080a0199d584ff8beb3540db
VictorGonzalezEscriba <Xavierfoster18>
2021-10-12 19:01:29 +0200

    added informe.txt and added changes to main

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib
 import scipy
 import pandas as pd
-
 from sklearn.datasets import make_regression
 from matplotlib import pyplot as plt
 import scipy.stats
+
+# Apartat (C): Analitzant Dades
 
 # Visualitzarem només 3 decimals per mostra
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -27,8 +28,28 @@ print("Dimensionalitat de la BBDD:", dataset.shape)
 print("Dimensionalitat de les entrades X", x.shape)
 print("Dimensionalitat de l'atribut Y", y.shape)
 
+"""
 print("Per comptar el nombre de valors no existents:")
-print(dataset.isnull().sum())
+print(dataset.isnull().sum().sum())
+# Para ver todas las columnas con valores vacios
 
-print("Per visualitzar les primeres 5 mostres de la BBDD:")
-print(dataset.head())
+x = dataset.isnull().sum()
+null_columns = dataset.columns[dataset.isnull().any()]
+print(null_columns)
+print(dataset[null_columns].isnull().sum())
+
+# VnMag 1, e.VbMAG 1, S280MAG 24, e.S280MA 24
+"""
+
+null_columns = dataset.columns[dataset.isnull().any()]
+
+for e in null_columns:
+    mean_value = dataset[e].mean()
+    dataset[e].fillna(value = mean_value, inplace=True)
+
+x = dataset.isnull().sum()
+null_columns = dataset.columns[dataset.isnull().any()]
+print(null_columns)
+print(dataset[null_columns].isnull().sum())
+
+print(dataset.isnull().sum().sum())
